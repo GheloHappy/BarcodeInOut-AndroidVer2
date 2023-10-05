a1958827c2f7c16868da9d64902ba8c866cd9774
Ghelonico <ghelonicomaligaya111@gmail.com>
2023-10-05 17:37:33 +0800

    Live v.1.5.11 inventory update

--- app/src/main/java/com/monheim/barcode_inout_v2/Inventory/InventoryFragment.java
+++ app/src/main/java/com/monheim/barcode_inout_v2/Inventory/InventoryFragment.java
@@ -260,55 +260,53 @@ public class InventoryFragment extends Fragment {
 //        });
 
         btnSave.setOnClickListener(v -> {
-            LinearLayout layout = new LinearLayout(getActivity());
-            layout.setOrientation(LinearLayout.VERTICAL);
+                LinearLayout layout = new LinearLayout(getActivity());
+                layout.setOrientation(LinearLayout.VERTICAL);
 
-            EditText etRefNbr = new EditText(getActivity());
-            etRefNbr.setHint("Reference number");
+                EditText etRefNbr = new EditText(getActivity());
+                etRefNbr.setHint("Reference number");
 
-            EditText etRemarks = new EditText(getActivity());
-            etRemarks.setHint("Remarks");
+                EditText etRemarks = new EditText(getActivity());
+                etRemarks.setHint("Remarks");
 
-            layout.addView(etRefNbr);
-            layout.addView(etRemarks);
+                layout.addView(etRefNbr);
+                layout.addView(etRemarks);
 
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMddyy");
-            String formattedDate = dateFormat.format(currentDate);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMddyy", Locale.getDefault());
+            String currentDateRef = dateFormat.format(c);
 
-            etRefNbr.setText(formattedDate);
-            etRefNbr.setEnabled(false);
-            etRemarks.setText(pubVars.GetInvtRemarks());
+                etRefNbr.setText(currentDateRef);
+                etRefNbr.setEnabled(false);
+                etRemarks.setText(pubVars.GetInvtRemarks());
 
-            new AlertDialog.Builder(getActivity())
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle("Input reference number")
-                    .setView(layout)
-                    .setPositiveButton("Yes",(dialog, which) -> {
-                        String refNbr = etRefNbr.getText().toString();
-                        String remarks = etRemarks.getText().toString();
+                new AlertDialog.Builder(getActivity())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Input reference number")
+                        .setView(layout)
+                        .setPositiveButton("Yes",(dialog, which) -> {
+                            String refNbr = currentDateRef;
+                            String remarks = etRemarks.getText().toString();
 
+                            pubVars.SetInvtReference(refNbr);
+                            pubVars.SetInvtRemarks(remarks);
 
-
-                        pubVars.SetInvtReference(refNbr);
-                        pubVars.SetInvtRemarks(remarks);
-
-                        if(refNbr.equals("")) {
-                            Toast.makeText(getActivity(), "Please add a reference number or text", Toast.LENGTH_SHORT).show();
-                            dialog.dismiss();
-                        } else {
-                            if(invtFunc.InsertInventory(refNbr, remarks)) {
-                                log.InsertUserLog(  "Inventory",refNbr);
-                                invtFunc.ClearTempInventory();
-                                BarcodeList(lvInventoryList);
-                                invtFunc.GetToTQtyCs(tvTotCs);
-                                invtFunc.GetToTQtyPcs(tvTotPcs);
+                            if(refNbr.equals("")) {
+                                Toast.makeText(getActivity(), "Please add a reference number or text", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
                             } else {
-                                Toast.makeText(getActivity(), "Failed to save inventory", Toast.LENGTH_SHORT).show();
+                                if(invtFunc.InsertInventory(refNbr, remarks)) {
+                                    log.InsertUserLog(  "Inventory",refNbr);
+                                    invtFunc.ClearTempInventory();
+                                    BarcodeList(lvInventoryList);
+                                    invtFunc.GetToTQtyCs(tvTotCs);
+                                    invtFunc.GetToTQtyPcs(tvTotPcs);
+                                } else {
+                                    Toast.makeText(getActivity(), "Failed to save inventory", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    })
-                    .setNegativeButton("CANCEL", null)
-                    .show();
+                        })
+                        .setNegativeButton("CANCEL", null)
+                        .show();
         });
 
         return rootView;
